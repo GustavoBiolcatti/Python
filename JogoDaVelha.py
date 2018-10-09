af1530279d3d779f05ffd3a3773ad8962c49d74f
Gustavo Gabriel Pereira Biolcatti <43222626+GustavoBiolcatti@users.noreply.github.com>
2018-10-09 20:52:59 -0300

    Update JogoDaVelha.py

--- JogoDaVelha.py
+++ JogoDaVelha.py
@@ -14,56 +14,101 @@ print("=" * 15)
 j1_nome = input("\nNome 1º jogador: ").strip().title()
 j2_nome = input("Nome 2º jogador: ").strip().title()
 
-tab = [[[], [], []], [[], [], []], [[], [], []]]
+tab = [[["-"], ["-"], ["-"]], [["-"], ["-"], ["-"]], [["-"], ["-"], ["-"]]]
+
 
 def ExibirJogo():
-    print(" {} | {} | {} ".format(tab[0][0][0], tab[0][1][0], tab[0][2][0]))
-    print("-"*11)
-    print(" {} | {} | {} ".format(tab[1][0][0], tab[1][1][0], tab[1][2][0]))
-    print("-"*11)
-    print(" {} | {} | {} ".format(tab[2][0][0], tab[2][1][0], tab[2][2][0]))
+    for i in range(1,4):
+        print(" {} | {} | {} ".format(tab[i-1][0][0], tab[i-1][1][0], tab[i-1][2][0]))
+        if i < 3:
+            print("-"*11)
 
-
-def InserirPosicao(l, c, t):
+            
+def InserirPosicao(t):
     valid_pos = True
     while valid_pos:
-        if 0 <= l <= 3 and 0 <= c <= 3:
-            valid_pos = False
-            tab[l][c] += t
-        else:
-            print("Posição fora de index! Digite novamente.")
-
-
+        
+        verif_input = True
+        while verif_input:
+            l = int(input("Posição na linha: "))
+            c = int(input("Posição na cluna: "))
+            
+            if 0 <= l <= 3 and 0 <= c <= 3:
+                if tab[l][c] != "X" and tab[l][c] != "O":
+                    tab[l][c] = t
+                    
+                    valid_pos = False
+                    verif_input = False
+                    continue
+                else:
+                    print("\nPosição já preenchida! Digite outra posição.\n")
+            else:
+                print("\nPosição fora de index! Digite novamente.\n")
+                
+    
+    print("\n\n{}".format(ExibirJogo()))
+    
+def VerifGanhou(nome):
+    if (tab[0][0][0] == "X" and tab[0][1][0] == "X" and tab[0][2][0] == "X") or (tab[0][0][0] == "O" and tab[0][1][0] == "O" and tab[0][2][0] == "O"):
+        print("Parabéns {}!".format(nome))
+        
+    elif (tab[1][0][0] == "X" and tab[1][1][0] == "X" and tab[1][2][0] == "X") or (tab[1][0][0] == "O" and tab[1][1][0] == "O" and tab[1][2][0] == "O"):
+        print("Parabéns {}!".format(nome))
+        
+    elif (tab[0][0][0] == "X" and tab[2][1][0] == "X" and tab[2][2][0] == "X") or (tab[2][0][0] == "O" and tab[2][1][0] == "O" and tab[2][2][0] == "O"):
+        print("Parabéns {}!".format(nome))
+    
+    elif (tab[0][0][0] == "X" and tab[1][0][0] == "X" and tab[2][0][0] == "X") or (tab[0][0][0] == "O" and tab[1][0][0] == "O" and tab[2][0][0] == "O"):
+        print("Parabéns {}!".format(nome))
+        
+    elif (tab[0][1][0] == "X" and tab[1][1][0] == "X" and tab[2][1][0] == "X") or (tab[0][1][0] == "O" and tab[1][1][0] == "O" and tab[2][1][0] == "O"):
+        print("Parabéns {}!".format(nome))
+        
+    elif (tab[0][2][0] == "X" and tab[1][2][0] == "X" and tab[2][2][0] == "X") or (tab[0][2][0] == "O" and tab[1][2][0] == "O" and tab[2][2][0] == "O"):
+        print("Parabéns {}!".format(nome))
+    
+    if (tab[0][0][0] == "X" and tab[1][1][0] == "X" and tab[2][2][0] == "X") or (tab[0][0][0] == "O" and tab[1][1][0] == "O" and tab[2][2][0] == "O"):
+        print("Parabéns {}!".format(nome))
+        
+    elif (tab[0][2][0] == "X" and tab[1][1][0] == "X" and tab[2][0][0] == "X") or (tab[0][2][0] == "O" and tab[1][1][0] == "O" and tab[2][0][0] == "O"):
+        print("Parabéns {}!".format(nome))
+        
+        
+jogo =  True
 jogadas = 1
-while jogadas <= 9:
+while jogo or jogadas <= 9:
     rodada = True
     while rodada:
+        
         verif_1 = True
         while verif_1:
             print("\nSua vez {}".format(j1_nome))
             tipo = "X"
-            l1 = int(input("Posição na linha: "))
-            c1 = int(input("Posição na cluna: "))
-
-            InserirPosicao(l1, c1, tipo)
+            
+            InserirPosicao(tipo)
+            VerifGanhou(j1_nome)
             verif_1 = False
+            
         verif_2 = True
         jogadas += 1
 
         if jogadas >= 9:
             break
+        
+        ''
 
         while verif_2:
             print("\nSua vez {}".format(j2_nome))
             tipo = "O"
-            l2 = int(input("Posição na linha: "))
-            c2 = int(input("Posição na cluna: "))
 
-            InserirPosicao(l2, c2, tipo)
+            InserirPosicao(tipo)
+            VerifGanhou(j1_nome)
+            
             verif_2 = False
         jogadas += 1
+        
+      
 
         rodada = False
 
-print("\n{}".format(ExibirJogo()))
-
+'''print("\n\n{}".format(ExibirJogo()))'''

7e9f27e351a8a258710d243ece315d2751e10d33
Gustavo Gabriel Pereira Biolcatti <43222626+GustavoBiolcatti@users.noreply.github.com>
2018-10-25 13:34:17 -0300

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="Calculo Salario",
-    version="0.8",
-    description=".py to .exe",
-    executables=[Executable("Calculo_Salario_Interface.py")]
-)
